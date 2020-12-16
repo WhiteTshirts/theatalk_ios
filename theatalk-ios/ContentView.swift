a189b84407d8233d2e6501d23cb52321c411f449
riku iwasaki <rikuiwasaki@rikunoMacBook-Pro.local>
2020-12-16 21:56:30 +0900

    initial commit

---
+++ theatalk-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  theatalk-ios
+//
+//  Created by riku iwasaki on 2020/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ theatalk-ios/theatalk_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  theatalk_iosApp.swift
+//  theatalk-ios
+//
+//  Created by riku iwasaki on 2020/12/16.
+//
+
+import SwiftUI
+
+@main
+struct theatalk_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
