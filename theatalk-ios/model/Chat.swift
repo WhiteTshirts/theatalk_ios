052efd0ef2f66e43b04b274c2b10ee8814a0fdb4
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-01-06 21:16:11 +0900

    chatとuserモデル追加

---
+++ theatalk-ios/model/Chat.swift
@@ -0,0 +1,16 @@
+//
+//  Chat.swift
+//  theatalk-ios
+//
+//  Created by riku iwasaki on 2021/01/06.
+//
+
+
+import Foundation
+import SwiftUI
+struct Chat:Hashable,Codable{
+    var user_id: Int
+    var room_id: Int
+    var test: String
+    
+}
--- theatalk-ios/model/User.swift
+++ theatalk-ios/model/User.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import SwiftUI
+struct User:Hashable,Codable{
+    var name: String
+    var tags: [Int]
+    
+    private var image_name: String
+    var image: Image{
+        Image(image_name)
+    }
+}
