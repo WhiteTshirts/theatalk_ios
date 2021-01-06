5662621e194da8e8de477e80852abe90e82a9657
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-01-06 21:54:06 +0900

    仮デザイン

--- theatalk-ios/view/Home.swift
+++ theatalk-ios/view/Home.swift
@@ -8,8 +8,37 @@
 import SwiftUI
 
 struct Home: View {
+    @State var textEntered = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+
+        VStack{
+            Text("theatalk")
+                .foregroundColor(Color.red)
+            HStack{
+                Rectangle().fill().frame(width:40,height: 40)//写真
+                TextField("検索",text:$textEntered)
+                Button(action:{
+                    print("Button Tapped")
+                }){
+                    Text("+")
+                        .font(.largeTitle)
+                        
+                }
+            }
+            ScrollView(.vertical){
+                VStack(alignment: .leading){
+                    ForEach(0..<10){_ in
+                        HStack(){
+                            Rectangle().fill(Color.red)
+                                .frame(width:140,height: 100)
+                            Rectangle().fill(Color.red)
+                                .frame(width:140,height: 100)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
