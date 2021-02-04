2948413a33305df7cf9ff70cbea51ed5a20d6917
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-02-04 20:48:05 +0900

    参加人数を画像ないに入れるようにした

--- theatalk-ios/view/Home.swift
+++ theatalk-ios/view/Home.swift
@@ -6,39 +6,75 @@
 //
 
 import SwiftUI
+import UIKit
 
+struct ActivityIndicator: UIViewRepresentable {
+    typealias UIViewType = UIActivityIndicatorView
+
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> ActivityIndicator.UIViewType {
+        UIActivityIndicatorView(style: style)
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
 extension UIScreen{
 
    static let screenWidth = UIScreen.main.bounds.size.width
    static let screenHeight = UIScreen.main.bounds.size.height
    static let screenSize = UIScreen.main.bounds.size
 }
+struct Sidemenu: View{
+    var body: some View{
+        Text("aaaaaa")
+    }
+}
 struct Home: View {
+    @State var loading = true
+    @State var info = false
     @ObservedObject var RoomsVM: RoomsViewModel
     @State var textEntered = ""
     @State var ProfileSize = CGSize(width: UIScreen.screenWidth/10, height: UIScreen.screenWidth/10)
     @State var RoomSize_Column2=CGSize(width: UIScreen.screenWidth/2.2, height: UIScreen.screenHeight/5)
     var body: some View {
-        NavigationView {
-            ScrollView{
-                ForEach(RoomsVM.rooms){room in
-                    HStack{
-                        NavigationLink(
-                            destination: ChatRoom(room:room)){
-                            RoomCell(room: room)
+        ZStack{
+            NavigationView {
+                ScrollView{
+                    ForEach(RoomsVM.rooms){room in
+                        HStack{
+                            NavigationLink(
+                                destination: ChatRoom(room:room)){
+                                RoomCell(room: room)
+                            }
                         }
                     }
+                }.onAppear{
+                    RoomsVM.load()
                 }
-            }.onAppear{RoomsVM.load()}
-            .navigationBarItems(leading:VStack{
-                HStack(alignment: .top){
-                    Rectangle().fill().frame(width: ProfileSize.width,height: ProfileSize.height)//写真
-                    TextField("検索",text:$textEntered)
-                    NavigationLink("+",destination:CreateRoom())
+                .navigationBarItems(leading:VStack{
+                    HStack(alignment: .top){
+                        Button(action: {
+                            info = true
+                        }){
+                            Rectangle().fill().frame(width: ProfileSize.width,height: ProfileSize.height)
+                        }
+                    
+                        TextField("検索",text:$textEntered)
+                        NavigationLink("+",destination:CreateRoom())
+                    }
                 }
+                .padding()
+                )
             }
-            .padding()
-            )
+            if(info){
+                Sidemenu()
+
+            }
+
         }
     }
 
@@ -46,8 +82,11 @@ struct Home: View {
 
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
-        Group {
+                ForEach(["iPhone SE", "iPhone X"], id: \.self) { deviceName in
             Home(RoomsVM: .init())
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
         }
     }
+    
 }
--- theatalk-ios/view/RoomCell.swift
+++ theatalk-ios/view/RoomCell.swift
@@ -22,23 +22,41 @@ struct RoomCell: View {
                     .opacity(0.3)
                     .frame(width: RoomSize_Column1.width,height: RoomSize_Column1.height)
                     
-            }.frame(width:RoomSize_Column1.width)
-            HStack(alignment: .top){
-                Text(room.name)
-                    .font(.caption)
-                    .foregroundColor(Color.black)
-                Text("参加人数\(room.viewer)人")
-                    .font(.caption)
-                    .foregroundColor(Color.black)
-            }
+            }.onFailure { e in
+                    // e: KingfisherError
+                    print("failure: \(e)")
+                }.frame(width:RoomSize_Column1.width)
+                .overlay(
+                    ZStack{
+                        
+                        HStack(alignment: .bottom){
+                            Text(" "+room.name)
+                                .font(.headline)
+                                .foregroundColor(Color.black)
+                            Text("参加人数\(room.viewer)人")
+                                .font(.headline)
+                                .foregroundColor(Color.red)
+                          
+                            
+                        
+                        
+                        }.background(Color.white).opacity(/*@START_MENU_TOKEN@*/0.8/*@END_MENU_TOKEN@*/)
+                    }
+
+                    ,alignment: .bottomTrailing
+                    
+                )
+  
         }
         
     }
+
+
 }
 
-//struct RoomCell_Previews: PreviewProvider {
-//    static var previews: some View {
-//        RoomCell(room: ModelData().rooms[0])
-//
-//    }
-//}
+struct RoomCell_Previews: PreviewProvider {
+    static var previews: some View {
+        RoomCell(room: Room(admin_id_:0,name_:"test1",id_:0,is_private_: false,start_time_: Date(), viewer_: 3,youtube_id_:"kgeG9kXFb0A"))
+
+    }
+}
