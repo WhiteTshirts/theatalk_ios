7b4b2bd2e5d78e2af6748e8b855d3c8840639a7c
rikutkb <iwasaki.riku.td@alumni.tsukuba.ac.jp>
2021-02-07 09:17:25 +0900

    ログイン画面の作成

--- theatalk-ios/view/login.swift
+++ theatalk-ios/view/login.swift
@@ -7,14 +7,46 @@
 
 import SwiftUI
 
-struct Login: View {
+struct LoginView: View {
+    @State var userName = ""
+    @State var password = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .center){
+            HStack{
+                Text("ユーザ名")
+                TextField("username", text: $userName)
+            }
+            HStack{
+                Text("パスワード")
+                SecureField("*******", text: $password)
+            }
+            Button(action: {
+                checkInput()
+            }) {
+                Text("決定")
+                    .foregroundColor(Color.red)
+                
+            }
+            if(userName == ""){
+                Text("ユーザ名が入力されていません")
+            }
+            if(password == ""){
+                Text("passwordが入力されていません")
+            }
+        
+
+
+        }.frame(width: 250, height: 300, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+
+    }
+    func checkInput(){
+
     }
 }
 
 struct Login_Previews: PreviewProvider {
     static var previews: some View {
-        Login()
+        LoginView()
     }
 }
